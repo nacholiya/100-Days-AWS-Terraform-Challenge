9233c9cbc41d625b0fc17edbdee9ba9c39f857e1
nacholiya <nacholiya@gmail.com>
2026-03-18 17:32:37 +0530

    terraform-day-34-state-bucket-1808-nacholiya

---
+++ Day-34-terraform-backend/main.tf
@@ -0,0 +1,45 @@
+data "aws_caller_identity" "current" {
+
+}
+
+resource "aws_s3_bucket" "state_bucket" {
+  bucket = "terraform-day-34-state-bucket-1808-nacholiya"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.state_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.state_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.state_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "state_lock_table" {
+  name         = "day-34-state-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
---
+++ Day-34-terraform-backend/terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+#   backend "s3" {
+#     bucket         = "terraform-day-34-state-bucket-1808-nacholiya"
+#     key            = "terraform.tfstate"
+#     region         = "ap-south-1"
+#     dynamodb_table = "day-34-state-lock-table"
+#   }
+}
