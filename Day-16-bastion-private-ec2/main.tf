f374606c5b0efd4fac8bfdfa52587a2d79550673
nacholiya <nacholiya@gmail.com>
2026-04-04 21:18:36 +0530

    fix the key pair issue for ci

--- Day-16-bastion-private-ec2/main.tf
+++ Day-16-bastion-private-ec2/main.tf
@@ -181,7 +181,7 @@ resource "aws_instance" "private_ec2" {
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.private_ec2_sg.id]
-  key_name               = aws_key_pair.bastion_key.key_name
+  key_name               = var.create_key_pair ? aws_key_pair.bastion_key[0].key_name : null
 
   tags = {
     Name = "day-16-private-ec2"
