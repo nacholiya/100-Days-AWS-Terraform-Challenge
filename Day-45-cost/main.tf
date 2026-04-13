f1422977911b70a913a3110c34e15f2855064d4e
nacholiya <nacholiya@gmail.com>
2026-04-13 11:50:44 +0530

    AWS Budget setup with alerts and cost monitoring using Terraform

---
+++ Day-45-cost/main.tf
@@ -0,0 +1,30 @@
+resource "aws_budgets_budget" "monthly_budget" {
+  name         = "Monthly Budget"
+  budget_type  = "COST"
+  limit_amount = 2
+  time_unit    = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 50
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = ["i.nikhil8088@gmail.com"]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 80
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = ["i.nikhil8088@gmail.com"]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = ["i.nikhil8088@gmail.com"]
+  }
+}
